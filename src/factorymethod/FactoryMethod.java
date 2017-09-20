d311781d7d5d178c3f7e24c8b11514e52099f7ac
Italo Bonfim <nsouzaitalo@hotmail.com>
2017-09-20 04:42:56 -0300

    Method Factory
    Atividade: Aplicaçoes Orientadas a Objetos.

---
+++ src/factorymethod/FactoryMethod.java
@@ -0,0 +1,17 @@
+package factorymethod;
+public class FactoryMethod {
+    
+    public static void main(String[] args) {
+        
+        FabricandoPessoas p1 = new FabricandoPessoas();
+        
+        String nome = "Salamandra";
+        String sexo = "Femi";
+        String nac = "Brasileira";
+        int idade = 19;
+        
+        p1.getPessoa(nome,sexo,nac,idade);
+        
+    }
+    
+}
